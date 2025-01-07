5b5b2c61df2c36b3bb56af5a7a098f4a1cb584e3
Fernando Balbino <dev.fernandobalbino@gmail.com>
2025-01-07 19:32:40 -0300

    end point

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,32 @@
 import database from "infra/database.js";
+import { version } from "react";
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+  const db_version_result = await database.query("SHOW server_version;");
+  const db_version_value = db_version_result.rows[0].server_version;
+
+  const db_max_connections = await database.query("SHOW max_connections;");
+
+  const db_name = process.env.POSTGRES_DB;
+
+  const db_max_connections_value = db_max_connections.rows[0].max_connections;
+
+  const db_opened_connections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [db_name],
+  });
+  const db_opened_connections_value = db_opened_connections.rows[0].count;
+  console.log(db_opened_connections_value);
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: db_version_value,
+        max_connections: db_max_connections_value,
+        opened_connections: db_opened_connections_value,
+      },
+    },
+  });
 }
 
 export default status;
